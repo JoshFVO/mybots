016f20f8130e4516c717324a72e3d2521de6de8d
Joshua Mahabir <kaizoku-o-gumi@FVOs-MacBook.local>
2023-01-09 16:42:58 -0600

    Finishing Part D

--- simulate.py
+++ simulate.py
@@ -1,9 +1,14 @@
 import pybullet as p
+import pybullet_data
 import time
 
 passphysicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
-p.loadSDF("box.sdf")
+p.setGravity(0,0,-9.8)
+
+p.loadSDF("boxes.sdf")
+planeId = p.loadURDF("plane.urdf")
 
 for i in range(1000):
     p.stepSimulation()
