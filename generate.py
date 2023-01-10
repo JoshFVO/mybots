868ea38cdaaeb25ef45815584965171895df237c
Joshua Mahabir <kaizoku-o-gumi@FVOs-MacBook.local>
2023-01-09 18:08:15 -0600

    Finishing Part E

--- generate.py
+++ generate.py
@@ -1,29 +1,25 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("boxes.sdf")
-
 length = 1
 width = 1
 height = 1
-
 x = 0
 y = 0
 z = 0.5
 
-#pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
-#pyrosim.Send_Cube(name="Box2", pos=[x+1,y,z+1] , size=[length,width,height])
+def Create_World():
+    pyrosim.Start_SDF("world.sdf")
+    pyrosim.Send_Cube(name="Box", pos=[x-5,y+5,z] , size=[length,width,height])
+    pyrosim.End()
 
-for i in range(5):
-    for j in range(5):
-        z = 0.5
-        length = 1
-        width = 1
-        height = 1
-        for k in range(10):
-            pyrosim.Send_Cube(name="Box", pos=[x + i, y + j,z] , size=[length,width,height])
-            z = z + 1
-            length = length * .9
-            width = width * .9
-            height = height * .9
+def Create_Robot():
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name="BackLeg", pos=[x,y,z] , size=[length,width,height])
+    pyrosim.Send_Joint(name = "BackLeg-Torso" , parent= "Torso" , child = "BackLeg" ,type = "revolute", position = [1,0,1])
+    pyrosim.Send_Cube(name="Torso", pos=[0,0,0.5] , size=[length,width,height])
+    pyrosim.Send_Joint(name = "FrontLeg-Torso" , parent= "Torso" , child = "FrontLeg" ,type = "revolute", position = [2,0,1])
+    pyrosim.Send_Cube(name="FrontLeg", pos=[0,0,-0.5] , size=[length,width,height])
+    pyrosim.End()
 
-pyrosim.End()
+Create_World()
+Create_Robot()
--- simulate.py
+++ simulate.py
@@ -7,8 +7,9 @@ p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
 p.setGravity(0,0,-9.8)
 
-p.loadSDF("boxes.sdf")
+p.loadSDF("world.sdf")
 planeId = p.loadURDF("plane.urdf")
+robotId = p.loadURDF("body.urdf")
 
 for i in range(1000):
     p.stepSimulation()
