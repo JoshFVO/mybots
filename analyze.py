3b8d8053b60f108ec4bfbf59d0500ae807e9d983
Joshua Mahabir <kaizoku-o-gumi@FVOs-MacBook.local>
2023-01-18 18:47:12 -0600

    Part G

--- analyze.py
+++ analyze.py
@@ -1,1 +1,16 @@
 import numpy
+import matplotlib.pyplot
+
+backLegSensorValues = numpy.load('/Users/kaizoku-o-gumi/Desktop/CS 396 - Artificial Life/mybots/data/backLegSensorValues.npy')
+frontLegSensorValues = numpy.load('/Users/kaizoku-o-gumi/Desktop/CS 396 - Artificial Life/mybots/data/frontLegSensorValues.npy')
+sinusoidalValuesBl = numpy.load('/Users/kaizoku-o-gumi/Desktop/CS 396 - Artificial Life/mybots/data/sinusoidalValuesBl.npy')
+sinusoidalValuesFl = numpy.load('/Users/kaizoku-o-gumi/Desktop/CS 396 - Artificial Life/mybots/data/sinusoidalValuesFl.npy')
+
+#matplotlib.pyplot.plot(backLegSensorValues, label='BackLeg', linewidth=4)
+#matplotlib.pyplot.plot(frontLegSensorValues, label='FrontLeg')
+matplotlib.pyplot.plot(sinusoidalValuesBl, linewidth=4)
+matplotlib.pyplot.plot(sinusoidalValuesFl)
+
+
+matplotlib.pyplot.legend()
+matplotlib.pyplot.show()
--- simulate.py
+++ simulate.py
@@ -1,8 +1,16 @@
 import pybullet as p
+import random
 import numpy
 import pyrosim.pyrosim as pyrosim
 import pybullet_data
 import time
+
+amplitudeBl = numpy.pi/4.0
+frequencyBl = 10.0
+phaseOffsetBl = 0
+amplitudeFl = numpy.pi/4.0
+frequencyFl = 10.0
+phaseOffsetFl = 0
 
 passphysicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -15,15 +23,29 @@ robotId = p.loadURDF("body.urdf")
 
 pyrosim.Prepare_To_Simulate(robotId)
 
-backLegSensorValues = numpy.zeros(100)
+backLegSensorValues = numpy.zeros(1000)
+frontLegSensorValues = numpy.zeros(1000)
 
-for i in range(100):
+targetAnglesBl = numpy.linspace(0, 2 * numpy.pi, 1000)
+targetAnglesFl = numpy.linspace(0, 2 * numpy.pi, 1000)
+for i in range(1000):
+    targetAnglesBl[i] = amplitudeBl * numpy.sin(frequencyBl * targetAnglesBl[i] + phaseOffsetBl)
+    targetAnglesFl[i] = amplitudeFl * numpy.sin(frequencyFl * targetAnglesFl[i] + phaseOffsetFl)
+
+#numpy.save('/Users/kaizoku-o-gumi/Desktop/CS 396 - Artificial Life/mybots/data/sinusoidalValuesBl.npy', targetAnglesBl)
+#numpy.save('/Users/kaizoku-o-gumi/Desktop/CS 396 - Artificial Life/mybots/data/sinusoidalValuesFl.npy', targetAnglesFl)
+
+
+for i in range(1000):
     p.stepSimulation()
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
-    #print(i)
-    time.sleep(1/60)
+    frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
+    pyrosim.Set_Motor_For_Joint(bodyIndex = robotId, jointName = "Torso_BackLeg", controlMode = p.POSITION_CONTROL, targetPosition = targetAnglesBl[i], maxForce = 25)
+    pyrosim.Set_Motor_For_Joint(bodyIndex = robotId, jointName = "Torso_FrontLeg", controlMode = p.POSITION_CONTROL, targetPosition = targetAnglesFl[i], maxForce = 25)
+    time.sleep(1/480)
 
 
-numpy.save('/Users/kaizoku-o-gumi/Desktop/CS 396 - Artificial Life/mybots/data/sensordata.npy', backLegSensorValues)
+numpy.save('/Users/kaizoku-o-gumi/Desktop/CS 396 - Artificial Life/mybots/data/backLegSensorValues.npy', backLegSensorValues)
+numpy.save('/Users/kaizoku-o-gumi/Desktop/CS 396 - Artificial Life/mybots/data/frontLegSensorValues.npy', frontLegSensorValues)
 
 p.disconnect()
